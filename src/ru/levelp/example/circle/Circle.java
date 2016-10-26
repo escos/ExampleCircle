06aa3f7f0536fda8a3223f7e93804248302192fd
escos <rv_kireev@bk.ru>
2016-10-26 19:11:55 +0300

    Update Circle.java

--- src/ru/levelp/example/circle/Circle.java
+++ src/ru/levelp/example/circle/Circle.java
@@ -4,6 +4,7 @@ public class Circle {
     double x;
     double y;
     double radius;
+    final static double E = 0.001;
 
     Circle(double radius, double x, double y) {
         if (radius < 0) {
@@ -34,19 +35,12 @@ public class Circle {
         radius += delta;
     }
 
-    public int identificatePointPlace(double x1, double y1) {
+    public boolean pointIntoCircle(double x1, double y1) {
         System.out.printf("Координаты заданной точки:x = %.3f, y = %.3f\n", x1, y1);
         double distance = Math.sqrt(Math.pow((x1 - x), 2) + Math.pow((y1 - y), 2));
-        if (distance * (1 - 0.001) > radius)
-            System.out.println("Точка находится за пределами круга");
-        else if (distance * (1 - 0.001) == radius) {
-            System.out.println("Точка находится на окружности");
-            return 0;
-        } else {
-            System.out.println("Точка находится внутри круга");
-            return 1;
-        }
-        return -1;
+        if (distance * (1 - E) > radius)
+            return false;
+        else return true;
     }
 
     public double circleLength() {
@@ -62,6 +56,6 @@ public class Circle {
     }
 
     public String toString() {
-        return "Обновленный радиус = " + this.radius + " , новые координаты центра: x = " + x + ", y = " + y;
+        return "Радиус окружности= " + this.radius + " , координаты центра: x = " + x + ", y = " + y;
     }
 }
